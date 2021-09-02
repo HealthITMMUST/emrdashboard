6cbbad05295c7509d9c9a335b37cac62d64e6c38
agesa3 <collinsagesa2000@gmail.com>
2021-09-02 14:28:44 +0300

    fixed the accounts chart

---
+++ src/views/Accounts/AccountCards.js
@@ -0,0 +1,250 @@
+import React from "react";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  Grid,
+  Typography
+} from "@material-ui/core";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import MoneyIcon from "@material-ui/icons/Money";
+import { red } from "@material-ui/core/colors";
+import { makeStyles } from "@material-ui/core/styles";
+import { green } from '@material-ui/core/colors';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import PeopleIcon from '@material-ui/icons/PeopleOutlined';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(2)
+  }
+}));
+const AccountCards = props => {
+      const classes = useStyles();
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={6} md={3}>
+        <Card sx={{ height: "100%" }} {...props}>
+          <CardContent>
+            <Grid
+              container
+              spacing={3}
+              sx={{ justifyContent: "space-between" }}
+            >
+              <Grid item>
+                <Typography color="textSecondary" gutterBottom variant="h6">
+                  BUDGET
+                </Typography>
+                <Typography color="textPrimary" variant="h3">
+                  $24,000
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Avatar
+                  sx={{
+                    backgroundColor: red[600],
+                    height: 56,
+                    width: 56
+                  }}
+                >
+                  <MoneyIcon />
+                </Avatar>
+              </Grid>
+            </Grid>
+            <Box
+              sx={{
+                pt: 2,
+                display: "flex",
+                alignItems: "center"
+              }}
+            >
+              <ArrowDownwardIcon sx={{ color: red[900] }} />
+              <Typography
+                sx={{
+                  color: red[900],
+                  mr: 1
+                }}
+                variant="body2"
+              >
+                12%
+              </Typography>
+              <Typography color="textSecondary" variant="caption">
+                Since last month
+              </Typography>
+            </Box>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+      <Card {...props}>
+    <CardContent>
+      <Grid
+        container
+        spacing={3}
+        sx={{ justifyContent: 'space-between' }}
+      >
+        <Grid item>
+          <Typography
+            color="textSecondary"
+            gutterBottom
+            variant="h6"
+          >
+            TOTAL CUSTOMERS
+          </Typography>
+          <Typography
+            color="textPrimary"
+            variant="h3"
+          >
+            1,600
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Avatar
+            sx={{
+              backgroundColor: green[600],
+              height: 56,
+              width: 56
+            }}
+          >
+            <PeopleIcon />
+          </Avatar>
+        </Grid>
+      </Grid>
+      <Box
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          pt: 2
+        }}
+      >
+        <ArrowUpwardIcon sx={{ color: green[900] }} />
+        <Typography
+          variant="body2"
+          sx={{
+            color: green[900],
+            mr: 1
+          }}
+        >
+          16%
+        </Typography>
+        <Typography
+          color="textSecondary"
+          variant="caption"
+        >
+          Since last month
+        </Typography>
+      </Box>
+    </CardContent>
+  </Card>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Card sx={{ height: "100%" }} {...props}>
+          <CardContent>
+            <Grid
+              container
+              spacing={3}
+              sx={{ justifyContent: "space-between" }}
+            >
+              <Grid item>
+                <Typography color="textSecondary" gutterBottom variant="h6">
+                  BUDGET
+                </Typography>
+                <Typography color="textPrimary" variant="h3">
+                  $24,000
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Avatar
+                  sx={{
+                    backgroundColor: red[600],
+                    height: 56,
+                    width: 56
+                  }}
+                >
+                  <MoneyIcon />
+                </Avatar>
+              </Grid>
+            </Grid>
+            <Box
+              sx={{
+                pt: 2,
+                display: "flex",
+                alignItems: "center"
+              }}
+            >
+              <ArrowDownwardIcon sx={{ color: red[900] }} />
+              <Typography
+                sx={{
+                  color: red[900],
+                  mr: 1
+                }}
+                variant="body2"
+              >
+                12%
+              </Typography>
+              <Typography color="textSecondary" variant="caption">
+                Since last month
+              </Typography>
+            </Box>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Card sx={{ height: "100%" }} {...props}>
+          <CardContent>
+            <Grid
+              container
+              spacing={3}
+              sx={{ justifyContent: "space-between" }}
+            >
+              <Grid item>
+                <Typography color="textSecondary" gutterBottom variant="h6">
+                  BUDGET
+                </Typography>
+                <Typography color="textPrimary" variant="h3">
+                  $24,000
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Avatar
+                  sx={{
+                    backgroundColor: red[600],
+                    height: 56,
+                    width: 56
+                  }}
+                >
+                  <MoneyIcon />
+                </Avatar>
+              </Grid>
+            </Grid>
+            <Box
+              sx={{
+                pt: 2,
+                display: "flex",
+                alignItems: "center"
+              }}
+            >
+              <ArrowDownwardIcon sx={{ color: red[900] }} />
+              <Typography
+                sx={{
+                  color: red[900],
+                  mr: 1
+                }}
+                variant="body2"
+              >
+                12%
+              </Typography>
+              <Typography color="textSecondary" variant="caption">
+                Since last month
+              </Typography>
+            </Box>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default AccountCards;
--- src/views/Accounts/Accounts.js
+++ src/views/Accounts/Accounts.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import AccountCards from "./AccountCards";
+import { colors } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import AccountCharts from "./AccountCharts";
+import { Box, Container, Grid } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    margin: 20
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center"
+  },
+  title: {
+    color: colors.indigo[500]
+  },
+  chartui: {
+    margin: 20
+  }
+}));
 
 const Accounts = () => {
-    return (
-        <div>
-            Accounts Page
-        </div>
-    )
-}
 
-export default Accounts
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className={classes.title}>Accounts</div>
+      <div className={classes.root}>
+        <AccountCards />
+      </div>
+      <div className={classes.chartui}>
+        <AccountCharts />
+      </div>
+      
+    </>
+  );
+};
+
+export default Accounts;
